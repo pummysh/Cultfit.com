178807bf18cd3937a860e3fa1b332c46f7505b45
PUMMY SHARMA <pummy9799@gmail.com>
2021-12-31 18:21:03 +0530

    google

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const connect = require('./configs/db');
 const port =process.env.PORT || 2345
-app.listen(process.env.PORT || 2345, function(){
+app.listen(process.env.PORT || 2345, async function(){
+    await connect();
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
